c9423953bbcd0ec16be19b296f6dc60dceaa9f24
ztiantian <243752773@qq.com>
2016-12-29 17:57:17 +0800

    up

--- pages/tabBar/tabBar.js
+++ pages/tabBar/tabBar.js
@@ -1,21 +1,36 @@
 Page({
     data:{
-        get:"获取本地数据缓存"
+        set:"保存本地数据缓存",
+        get:"获取本地数据缓存",
+        title:"改变窗口标题"
     },
-    getDate:function(){
-        // console.log(1)
-        wx.setNavigationBarTitle({
-            title:"当前页面"
-        })
+    // 保存数据
+    setDate:function(){
         wx.setStorage({
             key:"key",
-            data:'我是储存数据'
-        })
+            data:'我是tabBar页面储存数据'
+        });
+    },
+    // 获得保存数据
+    getDate:function(){
         wx.getStorage({
             key:"key",
             success:function(e){
                 console.log(e.data)
             }
-        })
+        });
+    },
+    // 设置标题
+    setTitle:function(){
+        wx.setNavigationBarTitle({
+            title:"我是改变的标题",
+            success:function(){
+                wx.hideNavigationBarLoading();//隐藏导航条加载动画。
+            },
+            fail:function(){
+                wx.showNavigationBarLoading();//在当前页面显示导航条加载动画。
+            }
+        });
+        
     }
 })
