8934e39b7548919c0d50e234694103cb646b518f
Yinzhishan <1772970517@qq.com>
2017-01-20 14:12:44 +0800

    card

--- vue/myztt/config/index.js
+++ vue/myztt/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    productionSourceMap: true,
+    productionSourceMap: true,//false是否是可以打包多个js
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
